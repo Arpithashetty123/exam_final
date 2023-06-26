8dc9448f50c4f5ceb3ef06a7a2bb45ae86bc9c31
arpitha shetty <shettyarpitha856@gmail.com>
2023-06-26 18:47:52 +0530

    hlf ui is completed

---
+++ lib/apps/features/utils/icon_widget.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget buildIcon(IconData icon,) {
+  return Icon(
+    icon,
+   size: 40,
+   color:Colors.grey.shade300
+  );
+}
